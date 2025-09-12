4ad2954b82a573e1875860d1f17b6a19516ce55d
azizp2 <aziezgt@gmail.com>
2025-09-12 21:49:58 +0700

    update api

--- MyAspNetApp/Program.cs
+++ MyAspNetApp/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/hello", () => "Hello from ASP.NET Core 8!");
+app.MapGet("/hello", () => "Hello from ASP.NET Core 8 -  Update Commit!");
 
 app.Run();
 
